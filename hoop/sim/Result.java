978c98becb62cee31ef90de76838b19b7ef99d99
Jose Rodriguez <joseocala@gmail.com>
2012-09-26 23:09:49 -0400

    Commited initial simulator.

---
+++ hoop/sim/Result.java
@@ -0,0 +1,17 @@
+package hoop.sim;
+
+public class Result {
+
+	public final String teamA;
+	public final String teamB;
+	public final int scoreA;
+	public final int scoreB;
+
+	public Result(String teamA, String teamB, int scoreA, int scoreB)
+	{
+		this.teamA = teamA;
+		this.teamB = teamB;
+		this.scoreA = scoreA;
+		this.scoreB = scoreB;
+	}
+}
