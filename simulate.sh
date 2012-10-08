41ac3561a1747e09aa16e0b2c5fca2366feed3b2
Jose Rodriguez <joseocala@gmail.com>
2012-10-08 10:33:49 -0400

    Updated simulate script.

--- simulate.sh
+++ simulate.sh
@@ -8,5 +8,5 @@ done
 for i in {1..100}
 do
   java hoop.sim.Hoop hoop/players.list 10 10 2 12 &> analysis/turn10self10.$i
-  tail -n 2 analysis/urn10self10.$i >> analysis/turn10self10
+  tail -n 2 analysis/turn10self10.$i >> analysis/turn10self10
 done
