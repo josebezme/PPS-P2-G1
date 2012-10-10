644c59807104c635a05b5d3b32d4bf06a7e65b18
Jose Rodriguez <joseocala@gmail.com>
2012-10-09 21:37:05 -0400

    Moded simulate script.

--- simulate.sh
+++ simulate.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-
+table=$1
 folder="analysis"
-echo "drop table if exists games;" > $folder/turn.sql
-echo "create table games (game_id integer, score integer, team varchar(255), self_games integer);" >> $folder/turn.sql
+echo "drop table if exists ${table};" > $folder/${table}.sql
+echo "create table ${table} (game_id integer, score integer, team varchar(255), self_games integer);" >> $folder/${table}.sql
 
-for self in 10
+for self in 100
 do
-  rm -f $folder/turn10self$self.sql
-  for i in {1..2}
+  echo "doing $1 with self: $self"
+  rm -f $folder/$table_turn10self$self.sql
+  for i in {1..25}
   do
-    java hoop.sim.Hoop hoop/players.list 10 $self 2 12 &> $folder/turn10self$self.$i
-    tail -n 2 $folder/turn10self$self.$i | 
-      awk -v gameid="$i" -v self="$self" '{ print "INSERT INTO games (game_id, score, team, self_games) VALUES (" gameid "," $1 ",'\''" $2 $3 $4 $5 $6 "'\''," self ");"}' >> $folder/turn10self$self.sql
+    echo $i
+    java hoop.sim.Hoop hoop/players.list 10 $self 100 12 &> $folder/${table}_turn10self$self.$i
+    tail -n 2 $folder/${table}_turn10self$self.$i | 
+      awk -v gameid="$i" -v self="$self" -v table="$1" '{ print "INSERT INTO " table " (game_id, score, team, self_games) VALUES (" gameid "," $1 ",'\''" $2 $3 $4 $5 $6 "'\''," self ");"}' >> $folder/$table_turn10self$self.sql
   done
-  cat analysis/turn10self$self.sql >> $folder/turn.sql
+  cat analysis/$table_turn10self$self.sql >> $folder/$table.sql
 done
